0d759320f07f7d423733c0f08a22dfd00a26f0dd
zacwilliamson <zacwilliamson@icloud.com>
2025-01-21 14:50:37 -0600

    pre-deployment

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,5 +2,34 @@
 pragma solidity 0.8.18;
 
 contract SimpleStorage {
-    
+    uint256 public myfavoriteNumber; // 0
+
+    uint256[] listOfFavoriteNumbers;
+
+    // similar to type/class
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // dynamic array
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myfavoriteNumber = _favoriteNumber;
+    }
+
+    // 'view' just means read the state, 'pure' returns the actual value
+    function retrieve() public view returns(uint256){
+        return myfavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    // 0xd9145CCE52D386f254917e481eB44e9943F39138
 }
